c08f2ff67602bc891ef77df0b536f9199ad4c4ad
HowcanoeWang <howcanoewang@gmail.com>
2020-08-05 09:25:39 +0800

    remove package examples folder

--- easypcp/example/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-cwd = os.getcwd()
-package_path = os.path.abspath(cwd.split('phenotypy')[0])
-print(f'[Pnt][__init__]Append "{package_path}" to system.path')
-sys.path.insert(0, package_path)
