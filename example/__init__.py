056be4870271dc57e333f08635137640d842792c
alex-feldman <49217604+alex-feldman@users.noreply.github.com>
2020-12-21 19:18:54 +0900

    Update __init__.py
    cwd.split(`phenotypy`) is useless, changed `package_path` to `cwd`
    Easypcp must be run from root folder, e.g.,
    `(easypcp37) C:\Users\Alex\Documents\GitHub\3Dphenotyping>python example\analysis.py`

--- example/__init__.py
+++ example/__init__.py
@@ -3,13 +3,8 @@ import os
 cwd = os.getcwd()
 '''
 package_path = os.path.abspath(cwd.split('phenotypy')[0])
-<<<<<<< Updated upstream
 print(f'[__init__]Append "{package_path}" to system.path')
 sys.path.insert(0, package_path)
-=======
-print(f'[Pnt][__init__]Append "{package_path}" to system.path')
-sys.path.insert(0, package_path)
 '''
-print(f'[Pnt][__init__]Append "{cwd}" to system.path')
+print(f'[__init__]Append "{cwd}" to system.path')
 sys.path.insert(0, cwd)
->>>>>>> Stashed changes
